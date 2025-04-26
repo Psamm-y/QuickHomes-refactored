5e9711a8e2c72eb71d7ba707143b957eae319c26
Psammy <ampadupsamuel1@gmail.com>
2025-04-26 17:21:15 +0000

    add utils and hero text

--- src/Home-page/Header.jsx
+++ src/Home-page/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import {
+  FaFacebookF,
+  FaHouseUser,
+  FaInstagram,
+  FaWhatsapp,
+} from 'react-icons/fa';
+import { PiHouseLineThin } from 'react-icons/pi';
+import { FaCircleUser, FaXTwitter } from 'react-icons/fa6';
+import { CiSearch } from 'react-icons/ci';
 const Header = () => {
   return (
     <>
@@ -10,10 +18,24 @@ const Header = () => {
         </div>
         <div className="flex gap-4 text-xs h-full items-center">
           <ul className="flex gap-2">
-            <li>f</li>
-            <li>t</li>
-            <li>i</li>
-            <li>w</li>
+            <li>
+              <a href="">
+                <FaFacebookF />
+              </a>
+            </li>
+            <li>
+              <FaXTwitter />
+            </li>
+            <li>
+              <a href="">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaWhatsapp />
+              </a>
+            </li>
           </ul>
           <button className="bg-green-700 h-full px-2 cursor-pointer">
             Add New Property
@@ -33,9 +55,24 @@ const Header = () => {
         </ul>
         <div className="flex gap-2">
           <ul className="flex gap-3">
-            <li>search</li>
-            <li>profile</li>
-            <li>house</li>
+            <li className="flex items-center">
+              <a href="" className="text-xl">
+                <CiSearch />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="" className="text-xl relative">
+                <PiHouseLineThin />
+                <span className="absolute text-xs bg-red-500 rounded-full w-4 h-4 text-center -top-2 -right-1 text-white">
+                  0
+                </span>
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="" className="text-2xl ml-4">
+                <FaHouseUser />
+              </a>
+            </li>
           </ul>
         </div>
       </nav>
--- src/utils/hero.js
+++ src/utils/hero.js
@@ -1,11 +1,15 @@
 export const heroText = {
   "page1": {
-    mainText:"",
-    subText:""
+    mainText:"Discover your dream house",
+    subText:"Why stress searching for which house fits your preference, needs and budget when you are one click away?"
   },
   "page2":
   {
-    mainText: "",
-    subText: ""
+    mainText: "Find the perfect land to build on",
+    subText: "You don't wanna make the mistake of your life buying a land situated where you are not comfortable with when you have vast optios to choose from"
+  },
+  "page3":{
+mainText:"Get the best office space",
+subText:"Looking for a right place to set up your business?"
   }
 }
