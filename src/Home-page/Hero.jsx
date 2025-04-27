47480616224bb4c58a5fa987dc200960908b0a19
Psammy <ampadupsamuel1@gmail.com>
2025-04-27 14:57:18 +0000

    add txt

--- src/Home-page/Hero.jsx
+++ src/Home-page/Hero.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import heroImage from '../assets/real-estate1-Photoroom.png';
 const Hero = () => {
   return (
-    <main className="grid grid-cols-[200px_1fr]">
-      <div className="border-1 min-h-16"></div>
-      <div></div>
+    <main className="grid grid-cols-[3fr_20em] p-16">
+      <div className="border-1 min-h-16">
+        <p>
+          Homes as <span>GRAND</span> as your Dreams
+        </p>
+        <p>Uncover rare spaces crafted for those who live without limits</p>
+        <div>
+          <button>Explore Listings </button>
+          <button>Get in touch</button>
+        </div>
+      </div>
+      <div className="border-1">
+        <img src={heroImage} alt="hero Image" />
+      </div>
     </main>
   );
 };
