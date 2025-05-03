4a67defcb558d4ff5454960b5a0ebcdc29de5e0a
Psammy <ampadupsamuel1@gmail.com>
2025-05-03 09:23:01 +0000

    add properties util

---
+++ src/utils/properties.js
@@ -0,0 +1,14 @@
+
+
+export const properties = [
+  {
+    description: "Light And Modern Apartment",
+    location: "Madina",
+    region: "Greater Accra",
+    rating:"4.76",
+    beds: "2",
+    bath: "1",
+    area: "2500",
+    price:"21000"
+  }
+]
